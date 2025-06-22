95b4eb859bc1c24c7c1376116a5f8b8c92a15d57
3114514 <1910577851@qq.com>
2025-06-22 11:51:27 +0800

    add touch supported(some games)

--- yxmb/91/ba.js
+++ yxmb/91/ba.js
@@ -4,12 +4,10 @@ import {Kyo} from './kyo.js';
 //主类
 class KOF {
     constructor(id) {
-        // jQuery选择器(id选择器)
         this.$kof = $('#' + id);
-
         this.game_map = new GameMap(this);
-
-        this.players = [//两名角色,用数组表示
+        
+        this.players = [
             new Kyo(this,{
                 id:0,
                 x:200,
@@ -26,7 +24,69 @@ class KOF {
                 height:200,
                 color:'red',
             }),
-        ]
+        ];
+        
+        // 初始化移动控制
+        this.init_mobile_controls();
+    }
+    
+    init_mobile_controls() {
+        // 移动端触控初始化
+        const outer = this;
+        
+        // 默认隐藏触控界面
+        $('#mobile-controls').hide();
+        
+        // 触控切换按钮
+        $('#mobile-toggle').on('click', function() {
+            const isVisible = $('#mobile-controls').is(':visible');
+            $('#mobile-controls').toggle(!isVisible);
+            $(this).toggleClass('active', !isVisible);
+            
+            // 更新按钮文本
+            $(this).text(isVisible ? '触控' : '关闭');
+        });
+        
+        // 绑定触控事件
+        $('[data-key]').on('touchstart mousedown', function(e) {
+            e.preventDefault();
+            const key = $(this).data('key');
+            outer.game_map.controller.addKey(key);
+            $(this).addClass('active');
+        });
+        
+        $('[data-key]').on('touchend mouseup mouseleave', function(e) {
+            e.preventDefault();
+            const key = $(this).data('key');
+            outer.game_map.controller.removeKey(key);
+            $(this).removeClass('active');
+        });
+        
+        // 防止页面滚动
+        $('body').on('touchmove', function(e) {
+            if ($('#mobile-controls').is(':visible')) {
+                e.preventDefault();
+            }
+        });
+        
+        // 在PC端也允许使用鼠标操作
+        $('[data-key]').on('mousedown', function(e) {
+            if ($('#mobile-controls').is(':visible')) {
+                e.preventDefault();
+                const key = $(this).data('key');
+                outer.game_map.controller.addKey(key);
+                $(this).addClass('active');
+            }
+        });
+        
+        $('[data-key]').on('mouseup mouseleave', function(e) {
+            if ($('#mobile-controls').is(':visible')) {
+                e.preventDefault();
+                const key = $(this).data('key');
+                outer.game_map.controller.removeKey(key);
+                $(this).removeClass('active');
+            }
+        });
     }
 }
 
--- yxmb/91/case.js
+++ yxmb/91/case.js
@@ -2,20 +2,28 @@
 export class Controller {
     constructor($canvas) {
         this.$canvas = $canvas;
-
         this.pressed_keys = new Set();
         this.start();
     }
 
     start() {
         let outer = this;
-        this.$canvas.keydown(function(e){//按下添加
+        this.$canvas.keydown(function(e){
             outer.pressed_keys.add(e.key);
-            console.log(e.key);
         });
 
-        this.$canvas.keyup(function(e){//按起删除
+        this.$canvas.keyup(function(e){
             outer.pressed_keys.delete(e.key);
         });
     }
-} 
+    
+    // 添加触控按键
+    addKey(key) {
+        this.pressed_keys.add(key);
+    }
+    
+    // 移除触控按键
+    removeKey(key) {
+        this.pressed_keys.delete(key);
+    }
+}
